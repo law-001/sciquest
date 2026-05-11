161e50a11db480f639e1ceb50d9fbb7d37e6b493
Mark Lawrence <markdumayag1@gmail.com>
2026-05-11 12:51:08 +0800

    test1

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -925,7 +925,8 @@ export function LandingPage({ onStartLearning, onAdminPortal, onNavigate }) {
               lineHeight: 1.55,
             }}
           >
-            Three big subjects, one playful place to explore them hello world.
+            Three big subjects, one playful place to explore them hello world
+            hello world.
           </p>
         </div>
 
